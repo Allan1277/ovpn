73700d27108d913158bd5f292c6c6bc518221c36
Allan1277 <55779507+Allan1277@users.noreply.github.com>
2023-07-15 10:23:25 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -374,7 +374,7 @@ sport = $ws_ovpn
 dport = $ovpn
 basic
 
-echo "<font color=\"green\"> NEWLEGENDS VPN (Lantin)</font>" > $loc/message
+echo "<font color=\"indigo\"> NEWLEGENDS VPN (Lantin)</font>" > $loc/message
 
 web=$loc/web
 mkdir $web 2> /dev/null
